4573c2933154193adefc66b2b17132c77a0e2c75
Martin Simard <Martin Simard>
2016-11-29 09:48:19 -0500

    Add nav search support

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,14 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function(e){
     e.preventDefault();
-    alert('Not yet wired up');
+
+    var location = this.refs.searchCity.value;
+    var encodedLocation = encodeURIComponent(location);
+
+    if(location.length > 0) {
+      this.refs.searchCity.value = '';
+      window.location.hash = '#/?location=' + encodedLocation;
+    }
   },
   render: function () {
     return (
@@ -27,7 +34,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search weather by city"/>
+                <input type="search" placeholder="Search weather by city" ref="searchCity"/>
               </li>
               <li>
                 <input type="submit" className="button" value="Get weather" />
